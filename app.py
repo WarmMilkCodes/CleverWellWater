84118ce66d7829dccc061dd42d28663e234ba0d1
WarmMilkCodes <mrguybanks@gmail.com>
2022-02-28 17:27:43 -0600

    Update

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import psycopg2
 import psycopg2.extras
 import io
 import csv
-#import xlwt
+import xlwt
 
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 # Development vs. Production Environment
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
@@ -28,14 +28,14 @@ if ENV == 'dev':
                             host=DB_HOST)
 else:
     app.debug = False
-    #DATABASE_URL = HEROKU URL HERE
+    DATABASE_URL = ('postgresql://ipboxaivgdyltp:0379a23d923e2a0b0e91308ac545be8b5a7ed72f14a041b4b31a480c671bd310@ec2-3-228-222-169.compute-1.amazonaws.com:5432/d6mome3tiuag88')
     
     
-    DB_HOST = NULL
-    DB_NAME = NULL
-    DB_USER = NULL
-    DB_PASS = NULL
-        
+    DB_HOST = 'ec2-3-228-222-169.compute-1.amazonaws.com'
+    DB_NAME = 'd6mome3tiuag88'
+    DB_USER = 'ipboxaivgdyltp'
+    DB_PASS = '0379a23d923e2a0b0e91308ac545be8b5a7ed72f14a041b4b31a480c671bd310'
+    
     conn = psycopg2.connect(dbname=DB_NAME, 
                         user=DB_USER, 
                         password=DB_PASS, 
@@ -50,74 +50,59 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def index():
     return render_template('index.html')
 
-'''
+
 @app.route('/admin')
 def adminPanel():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    s = "SELECT * FROM voting"
+    s = "SELECT * FROM results"
     cur.execute(s) # Execute the SQL
     list_users = cur.fetchall()
     return render_template('admin.html', list_users = list_users)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['loginName'] != 'PowerRankings' or request.form['password'] != 'roguevotes':
-            error = 'Invalid credentials. Please ensure username and password are correct.'
-        else:
-            return redirect(url_for('adminPanel'))
-    return render_template('login.html')
-'''
-
 @app.route('/index.html')
 def home():
     return render_template('index.html')
-'''
-@app.route('/success.html')
-def success():
-    return render_template('success.html')
-'''
+
 
 @app.route('/submit', methods=["POST"])
 def submit():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if request.method == 'POST':
-        date = request.form['date']
-        wellonereading = request.form['well-one-reading']
-        wellonefree = request.form['well-one-free']
-        welltworeading = request.form['well-two-reading']
-        welltwofree = request.form['well-two-free']
-        outsidereading = request.form['outsideeading']
-        outsidetotal = request.form['outsidetotal']
-        outsidefree = request.form['outsidefree']
+        collection_date = request.form['collection_date']
+        well_one_reading = request.form['well_one_reading']
+        well_one_free = request.form['well_one_free']
+        well_two_reading = request.form['well_two_reading']
+        well_two_free = request.form['well_two_free']
+        outside_reading = request.form['outside_reading']
+        outside_total = request.form['outside_total']
+        outside_free = request.form['outside_free']
 
 
-        if date == '' or wellonereading == '' or wellonetotal == '' or wellonefree == '' or welltworeading == '' or welltwototal == '' or welltwofree == '':
+        if collection_date == '' or well_one_reading == '' or well_one_free == '' or well_two_reading == '' or well_two_free == '':
                return render_template('index.html', message="Please fill all required fields.")
-        cur.execute("INSERT INTO voting (username, league, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (username, league, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10))
+        cur.execute("INSERT INTO results (collection_date, well_one_reading, well_one_free, well_two_reading, well_two_free, outside_reading, outside_total, outside_free) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (collection_date, well_one_reading, well_one_free, well_two_reading, well_two_free, outside_reading, outside_total, outside_free))
         conn.commit()
-        return redirect(url_for('success'))
+        return redirect(url_for('index'))
 
 
 @app.route('/delete/<string:id>', methods= ['POST','GET'])
 def deleteUser(id):
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute('DELETE FROM voting WHERE id = {0}'.format(id))
+        cur.execute('DELETE FROM results WHERE id = {0}'.format(id))
         conn.commit()
         flash('Record deleted.')
-        return redirect(url_for('adminPanel'))
+        return redirect(url_for('admin'))
 
 ### CSV DOWNLOAD 
-@app.route('/download_csv')
+@app.route('/download')
 def download():
-    return render_template('download_csv.html')
+    return render_template('download.html')
 
-@app.route('/download_csv/report/excel')
+@app.route('/download/report/excel')
 def download_report():
     curr = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    curr.execute("SELECT * FROM voting")
+    curr.execute("SELECT * FROM results")
     result = curr.fetchall()
     #for row in result:
         #print(row)
@@ -127,38 +112,31 @@ def download_report():
         #create WorkBook object
     workbook = xlwt.Workbook()
         #add sheet
-    sh = workbook.add_sheet('Power Rankings Report')
+    sh = workbook.add_sheet('Clever Well Water Report')
 
         #add headers
     sh.write(0, 0, 'ID')
-    sh.write(0, 1, 'Username')
-    sh.write(0, 2, 'League')
-    sh.write(0, 3, 'Team 1')
-    sh.write(0, 4, 'Team 2')
-    sh.write(0, 5, 'Team 3')
-    sh.write(0, 6, 'Team 4')
-    sh.write(0, 7, 'Team 5')
-    sh.write(0, 8, 'Team 6')
-    sh.write(0, 9, 'Team 7')
-    sh.write(0, 10, 'Team 8')
-    sh.write(0, 11, 'Team 9')
-    sh.write(0, 12, 'Team 10')
+    sh.write(0, 1, 'Collection Date')
+    sh.write(0, 2, 'Well One Reading')
+    sh.write(0, 3, 'Well One Free')
+    sh.write(0, 4, 'Well Two Reading')
+    sh.write(0, 5, 'Well Two Free')
+    sh.write(0, 6, 'Outside Reading')
+    sh.write(0, 7, 'Outside Total')
+    sh.write(0, 8, 'Outside Free')
+
 
     id = 0
     for row in result:
         sh.write(id+1,0, str(row['id']))
-        sh.write(id+1,1, row['username'])
-        sh.write(id+1,2, row['league'])
-        sh.write(id+1,3, row['team1'])
-        sh.write(id+1,4, row['team2'])
-        sh.write(id+1,5, row['team3'])
-        sh.write(id+1,6, row['team4'])
-        sh.write(id+1,7, row['team5'])
-        sh.write(id+1,8, row['team6'])
-        sh.write(id+1,9, row['team7'])
-        sh.write(id+1,10, row['team8'])
-        sh.write(id+1,11, row['team9'])
-        sh.write(id+1,12, row['team10'])
+        sh.write(id+1,1, row['Collection Date'])
+        sh.write(id+1,2, row['Well One Reading'])
+        sh.write(id+1,3, row['Well One Free'])
+        sh.write(id+1,4, row['Well Two Reading'])
+        sh.write(id+1,5, row['Well Two Free'])
+        sh.write(id+1,6, row['Outside Reading'])
+        sh.write(id+1,7, row['Outside Total'])
+        sh.write(id+1,8, row['Outside Free'])
         id += 1
 
     workbook.save(output)
